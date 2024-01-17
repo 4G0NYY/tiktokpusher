f700b5bf34119cdc70b9be4d3f65a06c5d258adb
4G0NYY <75240186+4G0NYY@users.noreply.github.com>
2024-01-17 18:49:49 +0100

    clean up

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# Simple TikTok Pusher.
+
+## How to use 
+
+1. install python (from the Microsoft Store, for Example)
+2. Change the path on line 11 in main.py to your specific Folder containing the videos
+3. Open a cmd window in this folder 
+4. Run ´pip install -r requirements.txt´
+5. (OPTIONAL) Change every how many Hours it will upload a video (by changing the number on line 20 of main.py)
+6. run the script with ´py main.py´
--- main.py
+++ main.py
@@ -4,33 +4,22 @@ import random
 import os
 from TikTokApi import TikTokApi
 
-# Create an instance of the TikTok API
 api = TikTokApi()
 
-# Function to upload a random video
 def upload_random_video():
-    # TODO: Replace 'video_folder' with the path to your video folder
+    # Change this to your specific Folder with Random Videos.
     video_folder = 'path/to/your/video/folder'
-    
-    # Get a list of all video files in the folder
     video_files = os.listdir(video_folder)
-    
-    # Choose a random video file
     random_video = random.choice(video_files)
-    
-    # Construct the full path to the selected video file
     video_path = os.path.join(video_folder, random_video)
-    
-    # TODO: Replace 'caption' with the desired caption for the video
+    # change 'Your video caption' to your desired caption (will be put on every single video)
     caption = 'Your video caption'
-    
-    # Upload the video
     api.upload_video(video_path, caption)
 
-# Schedule the random video upload every 4 hours
+# schedule, change the number to however many hours it will repeat
 schedule.every(4).hours.do(upload_random_video)
 
-# Run the scheduler
+# run an infinite loop
 while True:
     schedule.run_pending()
     time.sleep(1)
